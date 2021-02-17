73b0d24daee8edbf90d004bcb6a55d8ed3cb2cc1
yeongjunekoh <rhdudwns0@naver.com>
2021-02-17 13:40:40 +0900

    [Feat] Generate Todo Insert Component

--- App.js
+++ App.js
@@ -7,13 +7,14 @@ import {
   Text,
   ScrollView,
 } from 'react-native';
+import TodoInsert from './components/TodoInsert';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.appTitle}>Hello Todolist</Text>
       <View style={styles.card}>
-        <TextInput style={styles.input} placeholder="Add an item!" />
+        <TodoInsert />
         <ScrollView>
           <Text>TodoList</Text>
         </ScrollView>
--- components/TodoInsert.js
+++ components/TodoInsert.js
@@ -1,1 +1,41 @@
 // 텍스트 입력창과 추가 버튼
+// components/TodoInsert.js
+import React from 'react';
+import {Button, StyleSheet, TextInput, View} from 'react-native';
+
+const TodoInsert = () => {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.input}
+        placeholder="Add an item!"
+        placeholderTextColor={'#999'}
+        autoCorrect={false}
+      />
+      <View style={styles.button}>
+        <Button title={'ADD'} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+    padding: 20,
+    borderBottomColor: '#bbb',
+    borderBottomWidth: 1,
+    fontSize: 24,
+    marginLeft: 20,
+  },
+  button: {
+    marginRight: 10,
+  },
+});
+
+export default TodoInsert;
