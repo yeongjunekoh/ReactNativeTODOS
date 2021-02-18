65af6c99098329f01c34cffae48afea86505dbda
yeongjunekoh <rhdudwns0@naver.com>
2021-02-18 18:57:47 +0900

    [Feat] Generate TodoList Component

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,1 +1,20 @@
 // 추가된 아이템을 스크롤 뷰를 통해 보여주는 부분
+// components/TodoList.js
+import React from 'react';
+import {StyleSheet, ScrollView, Text} from 'react-native';
+
+const TodoList = () => {
+  return (
+    <ScrollView contentContainerStyle={styles.listContainer}>
+      <Text>TodoList</Text>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  listContainer: {
+    alignItems: 'center',
+  },
+});
+
+export default TodoList;
